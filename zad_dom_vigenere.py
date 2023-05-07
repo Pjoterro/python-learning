ea18ae5f127d83da3081f44f45ac6e734a49c9ef
Pjoter <59371425+Pjoterro@users.noreply.github.com>
2023-05-07 11:46:10 +0200

    Update zad_dom_vigenere.py
    Poprawianie testow

--- zad_dom_vigenere.py
+++ zad_dom_vigenere.py
@@ -19,44 +19,44 @@ def decrypt_vigenere(text, key):
 #        TESTY
 # ---------------------
 
-# Test 1:
+# Test encrypt_vigenere() 1:
 test_input_text = 'to jest test szyfrowania'
 test_input_key = 'ABCD'
 test_output_text = 'tp lhsu vhsu ucygtrwbpla'
 test_result = 'OK' if encrypt_vigenere(test_input_text, test_input_key) == test_output_text else 'NOT OK'
-print('Test 1: ' + test_result)
+print('Test encrypt_vigenere() 1: ' + test_result)
 
-# Test 2:
+# Test encrypt_vigenere() 2:
 test_input_text = 'atak odbedzie sie jutro. dzis o 16 dalsze rozkazy. jesli brak kontaktu - nic nie robic.'
 test_input_key = 'LEMON'
 test_output_text = 'lxmy bofqrmti ewr uyffb. odug b 16 oexgmp vanxldk. xrdpu pelo wcaeewhh - ymo bvp vapvn.'
 test_result = 'OK' if encrypt_vigenere(test_input_text, test_input_key) == test_output_text else 'NOT OK'
-print('Test 2: ' + test_result)
+print('Test encrypt_vigenere() 2: ' + test_result)
 
-# Test 3:
+# Test encrypt_vigenere() 3:
 test_input_text = 'nic nie powinno sie zmienic'
 test_input_key = 'A'
 test_output_text = 'nic nie powinno sie zmienic'
 test_result = 'OK' if encrypt_vigenere(test_input_text, test_input_key) == test_output_text else 'NOT OK'
-print('Test 3: ' + test_result)
+print('Test encrypt_vigenere() 3: ' + test_result)
 
-# Test 4:
+# Test decrypt_vigenere() 1:
 test_input_text = 'tp lhsu vhsu fhsaairpydnjc'
 test_input_key = 'ABCD'
 test_output_text = 'to jest test deszyfrowania'
 test_result = 'OK' if decrypt_vigenere(test_input_text, test_input_key) == test_output_text else 'NOT OK'
-print('Test 4: ' + test_result)
+print('Test decrypt_vigenere() 1: ' + test_result)
 
-# Test 5:
+# Test decrypt_vigenere() 2:
 test_input_text = 'ycqxes wfqlnxam! tmidhap qs 16. uln fqfbvfl.'
 test_input_key = 'HORNET'
 test_output_text = 'rozkaz przyjety! czekamy do 16. bez odbioru.'
 test_result = 'OK' if decrypt_vigenere(test_input_text, test_input_key) == test_output_text else 'NOT OK'
-print('Test 5: ' + test_result)
+print('Test decrypt_vigenere() 2: ' + test_result)
 
-# Test 6:
+# Test decrypt_vigenere() 3:
 test_input_text = 'powinno wyjsc to samo'
 test_input_key = 'AAAA'
 test_output_text = 'powinno wyjsc to samo'
 test_result = 'OK' if decrypt_vigenere(test_input_text, test_input_key) == test_output_text else 'NOT OK'
-print('Test 6: ' + test_result)
+print('Test decrypt_vigenere() 3: ' + test_result)
