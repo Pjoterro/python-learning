312dc051c4dfa6c7173a4226f9c61891e8b610ee
Pjoter <59371425+Pjoterro@users.noreply.github.com>
2023-05-10 11:47:23 +0200

    Update zad_dom_vigenere.py
    Poprawienie czytelnosci testow

--- zad_dom_vigenere.py
+++ zad_dom_vigenere.py
@@ -21,49 +21,43 @@ def vigenere_encryption(text, key):
 # ---------------------
 
 
-# Test vigenere_encryption() 1:
+# Test vigenere_encryption():
 test_input_text = 'to jest test szyfrowania'
 test_input_key = 'ABCD'
 test_output_text = 'tp lhsu vhsu ucygtrwbpla'
 test_result = 'OK' if vigenere_encryption(test_input_text, test_input_key) == test_output_text else 'NOT OK'
 print('Test vigenere_encryption() 1: ' + test_result)
 
-# Test vigenere_encryption() 2:
 test_input_text = 'atak odbedzie sie jutro. dzis o 16 dalsze rozkazy. jesli brak kontaktu - nic nie robic.'
 test_input_key = 'LEMON'
 test_output_text = 'lxmy bofqrmti ewr uyffb. odug b 16 oexgmp vanxldk. xrdpu pelo wcaeewhh - ymo bvp vapvn.'
 test_result = 'OK' if vigenere_encryption(test_input_text, test_input_key) == test_output_text else 'NOT OK'
 print('Test vigenere_encryption() 2: ' + test_result)
 
-# Test vigenere_encryption() 3:
 test_input_text = 'nic nie powinno sie zmienic'
 test_input_key = 'A'
 test_output_text = 'nic nie powinno sie zmienic'
 test_result = 'OK' if vigenere_encryption(test_input_text, test_input_key) == test_output_text else 'NOT OK'
 print('Test vigenere_encryption() 3: ' + test_result)
 
-# Test vigenere_encryption() 4:
 test_input_text = 'tp lhsu vhsu fhsaairpydnjc'
 test_input_key = '-ABCD'
 test_output_text = 'to jest test deszyfrowania'
 test_result = 'OK' if vigenere_encryption(test_input_text, test_input_key) == test_output_text else 'NOT OK'
 print('Test vigenere_encryption() 4: ' + test_result)
 
-# Test vigenere_encryption() 5:
 test_input_text = 'ycqxes wfqlnxam! tmidhap qs 16. uln fqfbvfl.'
 test_input_key = '-HORNET'
 test_output_text = 'rozkaz przyjety! czekamy do 16. bez odbioru.'
 test_result = 'OK' if vigenere_encryption(test_input_text, test_input_key) == test_output_text else 'NOT OK'
 print('Test vigenere_encryption() 5: ' + test_result)
 
-# Test vigenere_encryption() 6:
 test_input_text = 'powinno wyjsc to samo'
 test_input_key = '-AAAA'
 test_output_text = 'powinno wyjsc to samo'
 test_result = 'OK' if vigenere_encryption(test_input_text, test_input_key) == test_output_text else 'NOT OK'
 print('Test vigenere_encryption() 6: ' + test_result)
 
-# Test vigenere_encryption() 7:
 test_input_text = 'lrfwjwh dfuek'
 test_input_key = '-BARDZODLUGIKLUCZ'
 test_output_text = 'krotkie slowa'
